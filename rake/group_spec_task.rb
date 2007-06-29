0bb3db3396ea67f95bf38549e2917a81628b8b69
schmidt <schmidt@b232672f-569e-4225-9534-19f781cde581>
2007-06-29 07:28:42 +0000

    * moved trunk to branch to replace version 0.0.* - block based - with 0.1.* - method based
    git-svn-id: svn+ssh://rubyforge.org/var/svn/contextr/trunk@44 b232672f-569e-4225-9534-19f781cde581

--- rake/group_spec_task.rb
+++
@@ -1,7 +0,0 @@
-class GroupSpecTask < Spec::Rake::SpecTask
-  def initialize(group)
-    super(group) do |t|
-      t.spec_files = FileList["spec/#{group}/*_spec.rb"]
-    end
-  end
-end
--- rake/specific_group_spec_task.rb
+++
@@ -1,7 +0,0 @@
-class SpecificGroupSpecTask < Spec::Rake::SpecTask
-  def initialize(task, group)
-    super(task) do |t|
-      t.spec_files = FileList["spec/#{group}/#{task}_spec.rb"]
-    end
-  end
-end
