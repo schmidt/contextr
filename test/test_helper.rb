558f879d56b24e0184052552a831c2c13fdb3fa0
schmidt <schmidt@b232672f-569e-4225-9534-19f781cde581>
2007-09-27 14:38:18 +0000

    * added literate_markaby_test and migrated test_introduction to the new style
    git-svn-id: svn+ssh://rubyforge.org/var/svn/contextr/trunk@64 b232672f-569e-4225-9534-19f781cde581

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,9 @@
+require 'rubygems'
 require 'test/unit'
+require 'markaby'
+require 'ruby2ruby'
+require 'maruku'
+
 require File.dirname(__FILE__) + '/../lib/contextr'
 
 unless Object.const_defined?("ExampleTest")
@@ -62,3 +67,92 @@ unless Object.const_defined?("ExampleTest")
     include ExampleTest::ObjectExtension
   end
 end
+
+
+
+unless Object.const_defined?("LiterateMarkabyTest")
+  module LiterateMarkabyTest
+    TARGET_DIR = File.dirname(__FILE__) + "/../website/test/"
+    module ObjectExtension
+      def test(name, &block)
+        mab = Markaby::Builder.new
+
+        mab.test_class = Class.new(Test::Unit::TestCase)
+        mab.latest_test_case = 0
+
+        Object.const_set(name, mab.test_class) 
+        mab.xhtml_strict do
+          head do
+            title { name }
+          end
+          body do
+            h1 { name }
+            div(&block) 
+          end
+        end
+
+        Dir.mkdir(TARGET_DIR) unless File.directory?(TARGET_DIR)
+        File.open(TARGET_DIR + name.to_s.underscore + ".html", "w") do |f|
+          f.puts mab.to_s
+        end
+      end
+    end
+
+    module MarkabyBuilderExtension
+      attr_accessor :test_class, :latest_test_case
+      def output(&block)
+        block.call
+        self.pre(block.to_ruby.gsub(/^proc \{\n(.*)\n\}$/m, '\1'))
+      end
+      def example(&block)
+        name = "test_%03d" % (self.latest_test_case += 1)
+        test_class.class_eval do
+          define_method(name, &block)
+        end
+        self.pre(block.to_ruby.gsub(/^proc \{\n(.*)\n\}$/m, '\1'))
+      end
+    end
+    
+    module TestExtension
+      def assert_to_s(expected, actual)
+        assert_equal(expected, actual.to_s)
+      end
+
+      def result_of(object)
+        Result.new(object, self)
+      end
+
+      def output_of(object)
+        Output.new(object, self)
+      end
+
+      class Result 
+        attr_accessor :object, :test_class
+        def initialize(object, test_class)
+          self.object = object
+          self.test_class = test_class 
+        end
+        def ==(string)
+          test_class.assert_equal(string, object)
+        end
+      end
+      class Output < Result
+        def ==(string)
+          test_class.assert_equal(string, object.to_s)
+        end
+      end
+    end
+  end
+
+#  class Test::Unit::TestCase
+#    include LiterateMarkabyTest::TestExtension
+#  end
+  class Object 
+    include LiterateMarkabyTest::ObjectExtension
+  end
+  class Markaby::Builder
+    include LiterateMarkabyTest::MarkabyBuilderExtension
+  end
+end
+
+
--- test/test_introduction.rb
+++ test/test_introduction.rb
@@ -1,146 +1,160 @@
 require File.dirname(__FILE__) + "/test_helper.rb"
 
-test_class(:TestIntroduction)
+test(:TestIntroduction) do
 
-# Let's build a simple student's database.
-# Each University has a name and address. Each student has a name, address 
-# and an associated university.
-#
-# We are using a Struct to build our classes in an easy way. This provides all
-# getters, setters and an easy constructor setting all the instance variables.
-#
-# In order to get a nice output in our program we override the #to_s method
-# which is used in many cases by ruby, e.g. in Kernel#puts or in String 
-# interpolation.
-#
-# In most of the cases, the name is sufficient to represent each entity, i.e.
-# a student or a university.
+p %q{Let's build a simple student's database.
+     Each University has a name and address. Each student has a name, address 
+     and an associated university.}
 
-class University < Struct.new(:name, :address)
-  def to_s
-    name
+p %q{We are using a Struct to build our classes in an easy way. This provides 
+     all getters, setters and an easy constructor setting all the instance 
+     variables.}
+
+p %q{In order to get a nice output in our program we override the #to_s method
+     which is used in many cases by ruby, e.g. in Kernel#puts or in String 
+     interpolation.}
+
+p %q{In most of the cases, the name is sufficient to represent each entity, i.e.
+     a student or a university.}
+
+output do
+  class University < Struct.new(:name, :address)
+    def to_s
+      name
+    end
+  end
+
+  class Student < Struct.new(:name, :address, :university)
+    def to_s
+      name
+    end
   end
 end
 
-class Student < Struct.new(:name, :address, :university)
-  def to_s
-    name
+p %q{Under certain circumstances we would like to have a more verbose output.
+     This could mean print the university a student belongs to or attach the
+     address to the output.}
+
+h2 "Additonal methods"
+
+p %q{In a plain old Ruby project, this would result in additional methods, 
+     propably encapsulated in modules, that will be included into our classes. 
+     This allows reuse and better encapsulation.}
+
+output do
+  module AddressOutput
+    def to_s_with_address
+      "#{self} (#{self.address})"
+    end
+  end
+
+  class University
+    include AddressOutput
   end
 end
 
-# Under certain circumstances we would like to have a more verbose output.
-# This could mean print the university a student belongs to or attach the
-# address to the output.
+p %q{Now each university got a to_s_with_address method that could be called 
+     instead of to_s if you would like to have additional information.}
 
+output do
+  class Student
+    include AddressOutput
 
-# Additonal methods
-# =================
-
-# In a plain old Ruby project, this would result in additional methods, propably
-# encapsulated in modules, that will be included into our classes. This
-# allows reuse and better encapsulation.
-
-module AddressOutput
-  def to_s_with_address
-    "#{self} (#{self.address})"
+    def to_s_with_university
+      "#{self}; #{self.unversity}"
+    end
+    def to_s_with_university_and_address
+      "#{self.to_s_with_address}; #{self.unversity.to_s_with_address}"
+    end
   end
 end
 
-class University
-  include AddressOutput
+p %q{The same for each student. #to_s_with_unversity and 
+     #to_s_with_university_and_address give as well additional output.}
+
+p %q{So how can you use it. Let's create some instances first.}
+
+output do
+  $hpi = University.new("HPI", "Potsdam")
+  $gregor = Student.new("Gregor", "Berlin", $hpi)
 end
 
-# Now each university got a to_s_with_address method that could be called 
-# instead of to_s if you would like to have additional information.
+p %q{An now some output. This could live inside an erb template, a graphical 
+     ui or printed to the command line. In all these cases to_s is called 
+     automatically by the standard libary to receive a good representation of 
+     the object.}
 
-class Student
-  include AddressOutput
+p %q{The output method defined in test_helper.rb simulates this behaviour. All
+     examples are converted to test class automatically, so we can be sure, that
+     this document stays in sync with the libary.}
 
-  def to_s_with_university
-    "#{self}; #{self.unversity}"
-  end
-  def to_s_with_university_and_address
-    "#{self.to_s_with_address}; #{self.unversity.to_s_with_address}"
-  end
-end
-
-# The same for each student. #to_s_with_unversity and 
-# #to_s_with_university_and_address give as well additional output.
-#
-# So how can you use it. Let's create some instances first.
-
-$hpi = University.new("HPI", "Potsdam")
-$gregor = Student.new("Gregor", "Berlin", $hpi)
-
-# An now some output. This could live inside an erb template, a graphical ui or
-# printed to the command line. In all these cases to_s is called automatically
-# by the standard libary to receive a good representation of the object. 
-# The output method defined in test_helper.rb simulates this behaviour. All
-# examples are converted to test class automatically, so we can be sure, that
-# this document stays in sync with the libary.
-# 
-# puts gregor   # => prints "Gregor"
-# "#{gregor}"   # => evaluates to "Gregor"
-# <%= gregor %>   => as well as this
+code %q{
+ puts gregor   # => prints "Gregor"
+ "#{gregor}"   # => evaluates to "Gregor"
+ <%= gregor %>   => as well as this
+}
 
 example do
   output_of($gregor) == "Gregor"
   output_of($hpi) == "HPI"
 end
 
-# Assume, we would like to print an address list now.
+p %q{Assume, we would like to print an address list now.}
 
 example do
   output_of($gregor.to_s_with_address) == "Gregor (Berlin)"
 end
 
-# If you want a list with university and addresses, you would use 
-# #to_s_with_university_and_address. No automatic call to to_s anymore. If you
-# have your layout in an erb template, you have to change each and every 
-# occurrence of your variables.
+p %q{If you want a list with university and addresses, you would use 
+     #to_s_with_university_and_address. No automatic call to to_s anymore. If 
+     you have your layout in an erb template, you have to change each and every 
+     occurrence of your variables.}
 
 
-# Redefining to_s
-# ===============
+h2 "Redefining to_s"
 
-# To solve this problem you could redefine to_s on demand. I will demonstrate
-# this with some meta programming in a fresh class.
+p %q{To solve this problem you could redefine to_s on demand. I will demonstrate
+     this with some meta programming in a fresh class.}
 
-module GenericToS
-  def to_s
-    self.class.included_vars.collect do |var|
-      self.send(var)
-    end.join("; ")
-  end
+output do
+  module GenericToS
+    def to_s
+      self.class.included_vars.collect do |var|
+        self.send(var)
+      end.join("; ")
+    end
 
 
-  module ClassMethods
-    attr_accessor :included_vars
-    def set_to_s(*included_vars)
-      self.included_vars = included_vars
+    module ClassMethods
+      attr_accessor :included_vars
+      def set_to_s(*included_vars)
+        self.included_vars = included_vars
+      end
+    end
+
+    def self.included(base_class)
+      base_class.send(:extend, ClassMethods)
     end
   end
 
-  def self.included(base_class)
-    base_class.send(:extend, ClassMethods)
+  class Company < Struct.new(:name, :address)
+    include GenericToS
+  end
+
+  class Employee < Struct.new(:name, :address, :company)
+    include GenericToS
   end
 end
 
-class Company < Struct.new(:name, :address)
-  include GenericToS
+p %q{I will not go into detail how this code works, but I will show you how to 
+     use it. Let's get some instances first.}
+
+output do
+  $ms = Company.new("Microsoft", "Redmond")
+  $bill = Employee.new("Bill", "Redmond", $ms)
 end
 
-class Employee < Struct.new(:name, :address, :company)
-  include GenericToS
-end
-
-# I will not go into detail how this code works, but I will show you how to use
-# it. Let's get some instances first.
-
-$ms = Company.new("Microsoft", "Redmond")
-$bill = Employee.new("Bill", "Redmond", $ms)
-
-# And now use these instances.
+p "And now use these instances."
 
 example do
   Company.set_to_s(:name)
@@ -150,100 +164,113 @@ example do
   output_of($bill) == "Bill"
 end
 
-# Let's get the output including the addresses
+p "Let's get the output including the addresses"
+
 example do
   Employee.set_to_s(:name, :address)
 
   output_of($bill) == "Bill; Redmond"
 end
 
-# And including the employer
+p "And including the employer"
+
 example do
   Employee.set_to_s(:name, :address, :company)
 
   output_of($bill) == "Bill; Redmond; Microsoft"
 end
 
-# But hey. I wanted to have a list with all addresses, not just to employee's.
-# This should be an address list, right? But we did not tell the Company class
-# to print the address, but just the Employee class.
-#
-# So in our first approach, we had to change each place, where we use the
-# object. In the second approach we have to know all places where an address
-# is stored and apply the changes in there.
-#
-# By the way, what happens, if i was useing a multi-threaded application and
-# one user request a simple name list, and the other switches to an address list
-# in the meantime. Then the output will be mixed - with and without addresses.
-# This is not exactly what we want. So there has to be an easier, thread safe
-# solution.
+p %q{But hey. I wanted to have a list with all addresses, not just to 
+     employee's. This should be an address list, right? But we did not tell 
+     the Company class to print the address, but just the Employee class.}
+
+p %q{So in our first approach, we had to change each place, where we use the
+     object. In the second approach we have to know all places where an address
+     is stored and apply the changes in there.}
+
+p %q{By the way, what happens, if I was using a multi-threaded application and
+     one user request a simple name list, and the other switches to an address 
+     list in the meantime. Then the output will be mixed - with and without 
+     addresses. This is not exactly what we want. So there has to be an easier, 
+     thread safe solution.}
 
 
-# ContextR
-# ========
+h2 "ContextR"
 
-# This is were context-oriented programming comes into play. I will again start
-# from the scratch. It is not much and we all know the problem space now. 
+p %q{This is were context-oriented programming comes into play. I will again 
+     start from the scratch. It is not much and we all know the problem space 
+     now.}
 
-# The same setup, just another setting. First the basic implementation, just 
-# like we did it in our first approach
-class Religion < Struct.new(:name, :origin)
-  def to_s
-    name
+p %q{The same setup, just another setting. First the basic implementation, just 
+     like we did it in our first approach}
+
+output do
+  class Religion < Struct.new(:name, :origin)
+    def to_s
+      name
+    end
   end
-end
-class Believer < Struct.new(:name, :origin, :religion)
-  def to_s
-    name
-  end
-end
-
-# Now define the additional behaviour in separate modules. Please don't be 
-# scared because of the strange syntax and method calls.
-# yield(:receiver) refers to the "normal" self when these modules are included
-# yield(:next) is much like a super call.
-#
-# Future versions of ContextR will hopefully provide a nicer syntax here.
-module OriginMethods
-  def to_s
-    "#{yield(:next)} (#{yield(:receiver).origin})"
-  end
-end
-module ReligionMethods
-  def to_s
-    "#{yield(:next)}; #{yield(:receiver).religion}"
+  class Believer < Struct.new(:name, :origin, :religion)
+    def to_s
+      name
+    end
   end
 end
 
-# Finally we need to link our additional behaviour to our basic classes.
-# We also need to tell the framework, when this behaviour should be applied.
-class Religion
-  include OriginMethods => :location
-end
-class Believer
-  include OriginMethods => :location
-  include ReligionMethods => :believe
+p %q{Now define the additional behaviour in separate modules. Please don't be 
+     scared because of the strange syntax and method calls.
+     yield(:receiver) refers to the "normal" self when these modules are 
+     included yield(:next) is much like a super call.}
+
+p %q{Future versions of ContextR will hopefully provide a nicer syntax here.}
+
+output do
+  module OriginMethods
+    def to_s
+      "#{yield(:next)} (#{yield(:receiver).origin})"
+    end
+  end
+  module ReligionMethods
+    def to_s
+      "#{yield(:next)}; #{yield(:receiver).religion}"
+    end
+  end
 end
 
-# The additional context dependent behaviour is organised within layers. A 
-# single layer may span multiple classes - in this case the location layer does.
-# To enable the additional code, the programmes shall activate layers. A
-# layer activation is only effective within a block scope and within the 
-# current thread.
-#
-# Let's see, how it looks like when we use it.
+p %q{Finally we need to link our additional behaviour to our basic classes.
+     We also need to tell the framework, when this behaviour should be applied.}
 
-$christianity = Religion.new("Christianity", "Israel")
-$the_pope = Believer.new("Benedikt XVI", "Bavaria", $christianity)
+output do
+  class Religion
+    include OriginMethods => :location
+  end
+  class Believer
+    include OriginMethods => :location
+    include ReligionMethods => :believe
+  end
+end
+
+p %q{The additional context dependent behaviour is organised within layers. A 
+     single layer may span multiple classes - in this case the location layer 
+     does. To enable the additional code, the programmes shall activate layers.
+     A layer activation is only effective within a block scope and within the 
+     current thread.}
+
+p "Let's see, how it looks like when we use it."
+
+output do
+  $christianity = Religion.new("Christianity", "Israel")
+  $the_pope = Believer.new("Benedikt XVI", "Bavaria", $christianity)
+end
 
 example do
   output_of($christianity) == "Christianity"
   output_of($the_pope) == "Benedikt XVI"
 end
 
-# Would like to have an address? For this we have to activate the location 
-# layer. Now the additional behaviour defined within the layer, will be 
-# executed around the base method defined within the class
+p %q{Would like to have an address? For this we have to activate the location 
+     layer. Now the additional behaviour defined within the layer, will be 
+     executed around the base method defined within the class.}
 
 example do
   ContextR.with_layer :location do 
@@ -252,17 +279,18 @@ example do
   end
 end
 
-# Of course the additional behaviour is deactivated automatically after the
-# blocks execution.
+
+p %q{Of course the additional behaviour is deactivated automatically after the
+     blocks execution.}
 
 example do
   output_of($christianity) == "Christianity"
   output_of($the_pope) == "Benedikt XVI"
 end
 
-# Everything back to normal.
-#
-# Lets activate the believe layer
+p "Everything back to normal."
+
+p "Lets activate the believe layer"
 
 example do
   ContextR.with_layer :believe do 
@@ -270,9 +298,9 @@ example do
   end
 end
 
-# Now we need both, location and believe. How does it look like? You have to
-# options. You may activate the two one after the other or all at once. It 
-# is just a matter of taste, the result remains the same
+p %q{Now we need both, location and believe. How does it look like? You have to
+     options. You may activate the two one after the other or all at once. It 
+     is just a matter of taste, the result remains the same.}
 
 example do
   ContextR.with_layer :believe, :location do 
@@ -280,12 +308,12 @@ example do
   end
 end
 
-# As you can see, the activation of the location layer is operative in the
-# whole execution context of the block. Each religion prints its origin, wheter
-# to_s was called directly or indirectly.
-#
-# If you change your mind within your call stack, you may of course deactivate
-# layers again.
+p %q{As you can see, the activation of the location layer is operative in the
+     whole execution context of the block. Each religion prints its origin, 
+     wheter to_s was called directly or indirectly.}
+
+p %q{If you change your mind within your call stack, you may of course 
+     deactivate layers again.}
 
 example do
   ContextR.with_layer :believe do 
@@ -301,11 +329,12 @@ example do
   end
 end
 
-# These encaspulations may be as complex as your application. ContextR will
-# keep track of all activations and deactivations within the blocks and
-# restore the settings after the block was executed.
-#
-# This was just a short introduction on a problem case, that can be solved
-# with context-oriented programming. You have seen, the advantages and how
-# to use it. In other files in this folder, you can learn more on the dynamics
-# and meta programming interfaces of ContextR
+p %q{These encaspulations may be as complex as your application. ContextR will
+     keep track of all activations and deactivations within the blocks and
+     restore the settings after the block was executed.}
+
+p %q{This was just a short introduction on a problem case, that can be solved
+     with context-oriented programming. You have seen, the advantages and how
+     to use it. In other files in this folder, you can learn more on the 
+     dynamics and meta programming interfaces of ContextR.}
+end
