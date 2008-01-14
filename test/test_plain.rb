efbcc74c8ea9714ade8ac9d7c2a365e024138759
Gregor Schmidt <ruby@schmidtwisser.de>
2008-01-14 14:16:48 +0100

    removed all external dependencies from test_plain added rubinius' byte code files to ignore list

--- test/test_plain.rb
+++ test/test_plain.rb
@@ -1,5 +1,5 @@
-require File.dirname(__FILE__) + "/test_helper.rb"
-test_class(:TestPlain)
+require 'test/unit'
+require File.dirname(__FILE__) + '/../lib/contextr'
 
 class C1 < Struct.new(:a, :b)
   def to_s
@@ -35,52 +35,58 @@ end
 $c1 = C1.new("a1", "b1")
 $c2 = C2.new("a2", "b2", $c1)
 
-example do
-  output_of($c1) == "a1"
-  output_of($c2) == "a2"
-end
-
-example do
-  ContextR.with_layer :b do 
-    output_of($c1) == "a1 (b1)"
-    output_of($c2) == "a2 (b2)"
+class TestPlain < Test::Unit::TestCase
+  def test_001
+    assert_equal("a1", $c1.to_s)
+    assert_equal("a2", $c2.to_s)
   end
-end
 
-example do
-  output_of($c1) == "a1"
-  output_of($c2) == "a2"
-end
-
-example do
-  ContextR.with_layer :c do 
-    output_of($c2) == "a2; a1"
-  end
-end
-
-example do
-  ContextR.with_layer :b, :c do 
-    output_of($c2) == "a2 (b2); a1 (b1)"
-  end
-end
-
-example do
-  ContextR::with_layer :b do
-    ContextR.with_layer :c do 
-      output_of($c2) == "a2 (b2); a1 (b1)"
-
-      ContextR.without_layer :c do 
-        output_of($c2) == "a2 (b2)"
-      end
-
-      output_of($c2) == "a2 (b2); a1 (b1)"
+  def test_002
+    ContextR.with_layer :b do 
+      assert_equal("a1 (b1)", $c1.to_s)
+      assert_equal("a2 (b2)", $c2.to_s)
     end
   end
-end
 
-example(1.8) do
-  assert_equal(["to_s"], C1.in_layer(:b).instance_methods)
-end
-example(1.9) do
-  assert_equal([:to_s], C1.in_layer(:b).instance_methods)
+  def test_003
+    assert_equal("a1", $c1.to_s)
+    assert_equal("a2", $c2.to_s)
+  end
+
+  def test_004
+    ContextR.with_layer :c do 
+      assert_equal("a2; a1", $c2.to_s)
+    end
+  end
+
+  def test_005
+    ContextR.with_layer :b, :c do 
+      assert_equal("a2 (b2); a1 (b1)", $c2.to_s)
+    end
+  end
+
+  def test_006
+    ContextR::with_layer :b do
+      ContextR.with_layer :c do 
+        assert_equal("a2 (b2); a1 (b1)", $c2.to_s)
+
+        ContextR.without_layer :c do 
+          assert_equal("a2 (b2)", $c2.to_s)
+        end
+
+        assert_equal("a2 (b2); a1 (b1)", $c2.to_s)
+      end
+    end
+  end
+
+  if RUBY_VERSION =~ /1\.8/
+    def test_007
+      assert_equal(["to_s"], C1.in_layer(:b).instance_methods)
+    end
+  end
+  if RUBY_VERSION =~ /1\.9/
+    def test_007
+      assert_equal([:to_s], C1.in_layer(:b).instance_methods)
+    end
+  end
 end
